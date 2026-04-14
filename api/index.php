d69e78ea1034a101db7eeca9113046838b210e5a
FalahHP <falahhereg@gmail.com>
2026-04-14 12:42:38 +0700

    debug: check phpinfo on vercel

--- api/index.php
+++ api/index.php
@@ -1,10 +1,2 @@
 <?php
-
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    echo "<h1>Bootstrap Error</h1>";
-    echo "<p>Error: " . $e->getMessage() . "</p>";
-    echo "<p>File: " . $e->getFile() . " on line " . $e->getLine() . "</p>";
-    echo "<pre>" . $e->getTraceAsString() . "</pre>";
-}
+phpinfo();
