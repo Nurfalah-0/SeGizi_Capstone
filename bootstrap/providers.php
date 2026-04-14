ae070e4a5d187e28a19072ceb3ea4bc33da5950d
FalahHP <falahhereg@gmail.com>
2026-04-14 12:54:01 +0700

    fix(vercel): explicitly register View and Inertia providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,6 @@ use App\Providers\FortifyServiceProvider;
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
+    Inertia\ServiceProvider::class,
 ];
