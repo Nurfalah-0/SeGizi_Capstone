92653612f0920f56651a1279d11225ac125d7444
FalahHP <falahhereg@gmail.com>
2026-04-14 13:22:58 +0700

    debug: list files on vercel

--- api/index.php
+++ api/index.php
@@ -1,8 +1,15 @@
 <?php
-
-require __DIR__ . '/../vendor/autoload.php';
-$app = require __DIR__ . '/../bootstrap/app.php';
-
-use Illuminate\Http\Request;
-
-$app->handleRequest(Request::capture());
+echo "<h1>File Listing</h1><pre>";
+function listFiles($dir, $indent = "") {
+    $files = scandir($dir);
+    foreach ($files as $file) {
+        if ($file === "." || $file === "..") continue;
+        echo $indent . $file . (is_dir("$dir/$file") ? "/" : "") . "\n";
+        if (is_dir("$dir/$file") && $indent === "") {
+            listFiles("$dir/$file", $indent . "  ");
+        }
+    }
+}
+listFiles(dirname(__DIR__));
+echo "</pre>";
+die();
