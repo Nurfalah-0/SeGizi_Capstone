f4ee93697d771b7d08b2271f6a190141edb76760
FalahHP <falahhereg@gmail.com>
2026-04-14 20:47:53 +0700

    branding: revert to original NutriFlow logo for favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,8 +32,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon-nutriflow.png" type="image/png">
-        <link rel="apple-touch-icon" href="/favicon-nutriflow.png">
+        <link rel="icon" href="/logo-nutriflow-favicon.png" type="image/png">
+        <link rel="apple-touch-icon" href="/logo-nutriflow-favicon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
