d7ff8a20c8fa4f565bd5d863ebb08f3edba332da
FalahHP <falahhereg@gmail.com>
2026-04-14 12:36:31 +0700

    debug: add error catching to api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Bootstrap Error</h1>";
+    echo "<p>Error: " . $e->getMessage() . "</p>";
+    echo "<p>File: " . $e->getFile() . " on line " . $e->getLine() . "</p>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
