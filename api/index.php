be98642385f26a89953010d54562454a8ab2f044
FalahHP <falahhereg@gmail.com>
2026-04-15 14:16:13 +0700

    fix(vercel): run migrations on-the-fly for in-memory db

--- api/index.php
+++ api/index.php
@@ -4,5 +4,12 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
-$app->handleRequest(Request::capture());
+// Handle the request
+$request = Request::capture();
+
+// Run migrations on-the-fly for serverless in-memory DB
+Artisan::call('migrate', ['--force' => true]);
+
+$app->handleRequest($request);
